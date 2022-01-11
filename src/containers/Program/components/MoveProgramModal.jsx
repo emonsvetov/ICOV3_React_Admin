8c748335011afaa5354125a7cb1dd11429d13249
Arvind Kumar <arvind@inimisttech.com>
2022-01-11 22:46:55 +0530

    move program done

--- src/containers/Program/components/MoveProgramModal.jsx
+++ src/containers/Program/components/MoveProgramModal.jsx
@@ -3,6 +3,8 @@ import { Modal, ModalBody, ModalHeader, Button, ButtonToolbar } from 'reactstrap
 import ProgramTreeView from "./ProgramTreeView";
 import axios from 'axios'
 import {renameChildrenToSubrows} from '@/shared/helpers'
+import {useDispatch, sendModalFlashMessage, ModalFlashMessage} from "@/shared/components/flash"
+
 
 const fetchProgramData = async () => {
     try {
@@ -17,6 +19,7 @@ const fetchProgramData = async () => {
 };
 
 const MoveProgramModal = ({isOpen, setOpen, toggle, programId}) => {
+    const dispatch = useDispatch()
     const [formError, setFormError] = useState('');
     const [loading, setLoading] = useState(false);
     const [selected, setSelected] = useState([]);
@@ -38,26 +41,42 @@ const MoveProgramModal = ({isOpen, setOpen, toggle, programId}) => {
     const handleSelect = (event, nodeIds) => {
         setSelected(nodeIds)
     };
-    const onClickMoveConfrim = () => {
-        setFormError('')
+    const onClickMoveConfrim = async() => {
+        
         setLoading(true)
         // alert(JSON.stringify({selected, programId}))
-        console.log(programId)
-        console.log(selected)
+        // console.log(programId)
+        // console.log(selected)
         if( selected.length <=0 ) setFormError('Select a program to move to');
         else if( selected === programId )  setFormError('Select a different program to move to');
         else   {
-            alert('Allset')
+            try {
+                let formData = {
+                    program_id: selected
+                }
+                setLoading( true )
+                const response = await axios.patch(`/organization/1/program/${programId}`, formData);
+                console.log(response)
+                setLoading(false)
+                if( response.status === 200)    {
+                    dispatch(sendModalFlashMessage('Program has been moved', 'alert-success'))
+                }
+            } catch (e) {
+                setLoading(false)
+                dispatch(sendModalFlashMessage('Program could not be moved', 'alert-danger'))
+                throw new Error(`API error:${e?.message}`);
+            }
         }
         setLoading(false)
     };
     // console.log(data)
     data = renameChildrenToSubrows(data)
-    console.log(data)
+    // console.log(data)
     // console.log(selected)
     // console.log(originalSelected)
     return (
         <Modal className="modal-program" isOpen={isOpen} toggle={() => setOpen(true)}>
+            <ModalFlashMessage />
             <ModalHeader>
                 <h3 style={{"fontWeight": 500}}>Move Program to</h3>
             </ModalHeader>
