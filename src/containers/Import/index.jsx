8c8c30d287ee932c168eee62ef7b967225cb8334
olaccursed <olaccursed@yandex.ru>
2022-09-07 11:18:40 +0300

    ICO-128. page 2 only set the settings for the importer and import - Done.

--- src/containers/Import/index.jsx
+++ src/containers/Import/index.jsx
@@ -8,7 +8,7 @@ const ImportUser = () => (
     <Row>
       <Col md={12}>
         <h3 className="page-title">Import</h3>
-        <h3 class="page-subhead subhead"><Link className="" to="/">Home</Link> / <Link className="" to="/users">Import</Link></h3>
+        <h3 className="page-subhead subhead"><Link className="" to="/">Home</Link> / <Link className="" to="/users">Import</Link></h3>
       </Col>
     </Row>
     <Row>
