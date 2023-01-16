9ca4fc98edbd0ad647a4b5962783139a3edf4400
jaymoorejmcc <jaygdmoore@gmail.com>
2023-01-16 16:11:30 -0500

    Update AddProgramForm.jsx
    remove unwanted fields

--- src/containers/Program/components/AddProgramForm.jsx
+++ src/containers/Program/components/AddProgramForm.jsx
@@ -6,10 +6,10 @@ import CheckboxField from '@/shared/components/form/CheckboxField';
 import formValidation from "@/shared/validation/program-add";
 import axios from 'axios';
 
-const AddProgramForm = ( {program, organization} ) => {
+const AddProgramForm = ( {program, organization, auth} ) => {
 
     const hasValidValues = values => {
-      if( (values?.name && values.name.trim() !== "") && values.type && values.setup_fee) return true;
+      if( (values?.name && values.name.trim() !== "") && values.type) return true;
       return false;
     }
 
@@ -26,10 +26,10 @@ const AddProgramForm = ( {program, organization} ) => {
         // return;
         values = {...values, 
             ...{
-                // setup_fee: 100,
-                // is_pay_in_advance: 1, 
-                // invoice_for_awards: 0,
-                // is_add_default_merchants: 1,
+                setup_fee: 0,
+                is_pay_in_advance: 1,
+                invoice_for_awards: 0,
+                is_add_default_merchants: 1,
                 parent_id: program ? program.id : null,
                 factor_valuation: 40,
                 is_demo: 0
@@ -64,8 +64,8 @@ const AddProgramForm = ( {program, organization} ) => {
         validate={(values) => formValidation.validateForm(values)}
         initialValues={{
             name: program ? program.name + ' copy' : '',
-            setup_fee: 100,
-            is_pay_in_advance: false, 
+            setup_fee: 0,
+            is_pay_in_advance: true,
             invoice_for_awards: false,
             is_add_default_merchants: false
         }}
@@ -121,45 +121,22 @@ const AddProgramForm = ( {program, organization} ) => {
                 </Col>
             </Row>
         </div>
-
-        <Field name="setup_fee">
-        {({ input, meta }) => (
-            <div className="form__form-group">
-                <span className="form__form-group-label">Setup Fee</span>
-                <div className="form__form-group-field">
-                    <div className="form__form-group-row">
-                        <input type="text" {...input} placeholder="Setup Fee" />
-                        {meta.touched && meta.error && <span className="form__form-group-error">{meta.error}</span>}
-                    </div>
-                </div>
-            </div>
-        )}
-        </Field>
-        <Row className='w100'>
-          <Col md="6" lg="6" xl="6">
-            <div className="form__form-group">
-                <CheckboxField 
-                    name="is_pay_in_advance"
-                    label="Pay in Advance"
-                />
-            </div>
-          </Col>
-          <Col md="6" lg="6" xl="6">
-            <div className="form__form-group">
-                <CheckboxField 
-                    name="invoice_for_awards"
-                    label="Invoice for awards"
-                />
-            </div>
-          </Col>
-        </Row>
-
-        <div className="form__form-group">
-            <CheckboxField 
-                name="is_add_default_merchants"
-                label="Add default merchants to program"
-            />
-        </div>
+            {auth?.isSuperAdmin &&
+                <Field name="setup_fee">
+                    {({input, meta}) => (
+                        <div className="form__form-group">
+                            <span className="form__form-group-label">Setup Fee</span>
+                            <div className="form__form-group-field">
+                                <div className="form__form-group-row">
+                                    <input type="text" {...input} placeholder="Setup Fee"/>
+                                    {meta.touched && meta.error &&
+                                        <span className="form__form-group-error">{meta.error}</span>}
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </Field>
+            }
 
         <button type="submit" className="btn btn-primary account__btn account__btn--small" disabled={loading || pristine || !hasValidValues(values)}>Create Program</button>
       </form>
