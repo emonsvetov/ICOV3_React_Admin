812597bc9737252d4c3f9113c774a255104140ca
crypopanda430 <panda.kk1114@gmail.com>
2023-11-07 11:21:04 -0300

    add email 2fa

--- src/containers/LogIn/components/LogInForm.jsx
+++ src/containers/LogIn/components/LogInForm.jsx
@@ -16,10 +16,12 @@ const LogInForm = () => {
 
   const [errors, setErrors] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  const [code, setCode] = useState('');
+  const [google2faUrl, setGoogle2faUrl] = useState('');
+  const [message, setMessage] = useState('');
   const onSubmit = async values => {
 
-    // console.log(values);
+    console.log('onSubmit called', values);
 
     axios.post('/login', values)
     .then( (res) => {
@@ -27,8 +29,10 @@ const LogInForm = () => {
       // return;
       // console.log(res.status == 200)
       if(res.status == 200)  {
-        login(res.data)
-        var t = setTimeout(window.location = '/', 500)
+        const google2fa_url = res.data.google2fa_url;
+        setGoogle2faUrl(google2fa_url);
+        setErrors('Check your email for the 2FA code.');
+        // var t = setTimeout(window.location = '/', 500)
       }
     })
     .catch( error => {
@@ -38,12 +42,34 @@ const LogInForm = () => {
     })
   };
 
+  const handleVerify2FA = async value => {
+
+    console.log('2FA called');
+    const email = value.email;
+    const response = await fetch('/api/verify-2fa', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, code }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setMessage(data.message);
+      
+    } else {
+      setMessage(data.error);
+    }
+  };
+
   return (
   <Form
     onSubmit={onSubmit}
     validate={validate}
     render={({ handleSubmit, form, submitting, pristine, values }) => (
-    <form className="form" onSubmit={handleSubmit}>
+    <form className="form" onSubmit={google2faUrl ? handleVerify2FA : handleSubmit}>
       {
         errors && <ApiErrorMessage className="alert alert-danger fade show w100" errors={errors} />
       }
@@ -82,6 +108,29 @@ const LogInForm = () => {
           </div>
         )}
       </Field>
+      {google2faUrl ? (
+         <Field name="password">
+         {({ input, meta }) => (
+           <div className="form__form-group">
+             <span className="form__form-group-label">Password</span>
+               <div className="form__form-group-field">
+                 <div className="form__form-group-icon">
+                   <LockOutlineIcon />
+                 </div>
+                 <div className="form__form-group-row">
+                   <input type="password" {...input} placeholder="Password" />
+                   {meta.touched && meta.error && <span className="form__form-group-error">{meta.error}</span>}
+                 </div>
+             </div>
+             <div className="account__forgot-password">
+               <a href="/forgot">Forgot a password?</a>
+             </div>
+           </div>
+         )}
+       </Field>
+      ) : (
+        <button type="submit" className="btn btn-primary account__btn account__btn--small" disabled={loading}>Log In</button>
+      )}
       {/* <div className="form__form-group">
         <div className="form__form-group-field">
           <Field
@@ -91,7 +140,7 @@ const LogInForm = () => {
           />
         </div>
       </div> */}
-      <button type="submit" className="btn btn-primary account__btn account__btn--small" disabled={loading}>Log In</button>
+    
       {/* <Link className="btn btn-primary account__btn account__btn--small" to="/pages/one">Sign In</Link> */}
       <Link className="btn btn-outline-primary account__btn account__btn--small" to="/signup">Create Account</Link>
       </form>
