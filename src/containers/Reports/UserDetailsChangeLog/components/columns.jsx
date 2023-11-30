c670701f06d372c1ae5fcef4f5395a9258d0b34e
ashwani kumar <ashwanikumar@inimist.com>
2023-11-30 09:58:58 +0530

    user details change logs component created

---
+++ src/containers/Reports/UserDetailsChangeLog/components/columns.jsx
@@ -0,0 +1,31 @@
+
+
+export const TABLE_COLUMNS = [
+    {
+        Header: "Program ID",
+        accessor: "id",
+        Footer:"#",
+        disableFilters:true,
+        disableGlobalFilters:true,
+        // sticky:'left'
+    },
+    {
+        Header: "Program Name",
+        accessor: "name",
+        Footer:"Program Name",
+        Cell: ({ row, value }) => { return <Link to={`/program/view/${row.original.id}`}>{value}</Link>},
+    },
+    {
+        Header: "Root Program ID",
+        accessor: "root_id",
+        Footer: "Status",
+        filter: 'equals'
+    },
+    {
+        Header: "Root Program Name",
+        accessor: "root_name",
+        Footer: "Status",
+        filter: 'equals'
+    },
+]
+
