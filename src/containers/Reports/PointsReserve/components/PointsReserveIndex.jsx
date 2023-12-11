d01db215b420789f90ef24134d74d4d687e385fb
crypopanda430 <panda.kk1114@gmail.com>
2023-12-11 15:58:19 -0300

    add point reserve

--- src/containers/Reports/PointsReserve/components/PointsReserveIndex.jsx
+++ src/containers/Reports/PointsReserve/components/PointsReserveIndex.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Card, CardBody, Col } from 'reactstrap';
 import PointsReserveTable from './PointsReserveTable';
+import {isEmpty} from '@/shared/helpers'
+import { getAllPrograms } from '@/shared/apiHelper.jsx';
+import {connect} from "react-redux";
 
-const PointsReserveIndex = () => {
+const PointsReserveIndex = ({organization}) => {
   // const reactTableData = GetIndexData();
+  const [defaultPrograms, setDefaultPrograms] = useState([]);
+
+  useEffect(() => {
+    if ( isEmpty(defaultPrograms) ){
+      getAllPrograms( "minimal=true&limit=99999999" )
+          .then( response => {
+            const data = response?.data ? response.data : [];
+            const result = data.map(x => x.account_holder_id)
+            setDefaultPrograms(result);
+          })
+    }
+  })
+
+  if (isEmpty(defaultPrograms)) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <Col md={12}>
       <Card>
         <CardBody>
-          <PointsReserveTable />
+          <PointsReserveTable  programs={defaultPrograms}  />
         </CardBody>
       </Card>
     </Col>
   )
 }
 
-export default PointsReserveIndex;
+
+const mapStateToProps = (state) => {
+  return {
+    organization: state.organization,
+  };
+};
+export default connect(mapStateToProps)(PointsReserveIndex);
