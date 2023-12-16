ea0acb8479e9a60aa899668bd696656b57ccc656
Vicky0603 <victoria.kovalyova14@gmail.com>
2023-12-16 18:50:16 -0300

    Admin General: Unassigned Program Domains

--- src/containers/Reports/UnassignedProgramDomains/components/UnassignedProgramDomainTable.jsx
+++ src/containers/Reports/UnassignedProgramDomains/components/UnassignedProgramDomainTable.jsx
@@ -1,69 +1,27 @@
-import React, {useState, useEffect, useMemo} from "react";
-import { useTable, usePagination, useSortBy, useExpanded, useResizeColumns, useFlexLayout } from "react-table";
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
-import {TABLE_COLUMNS} from "./columns";
-import SortIcon from 'mdi-react/SortIcon';
-import SortAscendingIcon from 'mdi-react/SortAscendingIcon';
-import SortDescendingIcon from 'mdi-react/SortDescendingIcon';
+import React, {useEffect, useMemo, useState} from "react";
+import {useExpanded,  usePagination, useResizeColumns, useSortBy, useTable} from "react-table";
+import {QueryClient, QueryClientProvider, useQuery} from 'react-query'
 import ReactTablePagination from '@/shared/components/table/components/ReactTablePagination';
-import UnassignedProgramDomainFilter  from "./UnassignedProgramDomainFilter";
-import { Col, Row} from 'reactstrap';
-import axios from 'axios';
-import {isEqual, clone, cloneDeep} from 'lodash';
+import {Col, Row} from 'reactstrap';
 
-import {renameChildrenToSubrows} from '@/shared/helpers'
-import TrialBalanceFilter from "../../TrialBalance/components/TrialBalanceFilter";
+import {TABLE_COLUMNS} from "./columns";
+
 import {connect} from "react-redux";
-import {fetchApiData, fetchApiDataExport, useEffectToDispatch} from "../../../../shared/apiTableHelper";
-import Sorting from "../../../Example/sorting";
+import {
+    reducer,
+    useEffectToDispatch,
+    fetchApiData,
+    fetchApiDataExport,
+    initialState,
+
+    Sorting
+} from "@/shared/apiTableHelper"
+
+import { clone} from 'lodash';
+import UnassignedProgramDomainFilter from "./UnassignedProgramDomainFilter";
 
 const queryClient = new QueryClient()
 
-const initialState = {
-    queryPageIndex: 0,
-    queryPageSize: 10,
-    totalCount: null,
-    queryPageFilter:{},
-    queryPageSortBy: [],
-};
-
-const PAGE_CHANGED = 'PAGE_CHANGED';
-const PAGE_SIZE_CHANGED = 'PAGE_SIZE_CHANGED';
-const PAGE_SORT_CHANGED = 'PAGE_SORT_CHANGED'
-const PAGE_FILTER_CHANGED = 'PAGE_FILTER_CHANGED';
-const TOTAL_COUNT_CHANGED = 'TOTAL_COUNT_CHANGED';
-
-const reducer = (state, { type, payload }) => {
-    switch (type) {
-        case PAGE_CHANGED:
-            return {
-                ...state,
-                queryPageIndex: payload,
-            };
-        case PAGE_SIZE_CHANGED:
-            return {
-                ...state,
-                queryPageSize: payload,
-            };
-        case PAGE_SORT_CHANGED:
-            return {
-                ...state,
-                queryPageSortBy: payload,
-            };
-        case PAGE_FILTER_CHANGED:
-            return {
-                ...state,
-                queryPageFilter: payload,
-            };
-        case TOTAL_COUNT_CHANGED:
-            return {
-                ...state,
-                totalCount: payload,
-            };
-        default:
-            throw new Error(`Unhandled action type: ${type}`);
-    }
-};
 const DataTable = ({organization, programs}) => {
     const [filter, setFilter] = useState({
         programs: programs,
