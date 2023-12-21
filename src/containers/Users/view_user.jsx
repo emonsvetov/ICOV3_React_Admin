11cfafa9a2e63a77e65c96d4d992c9bb8f96a376
andrey <andreamelkov@yandex.ru>
2023-12-21 22:09:05 +0300

    ICOV3-159. Add User info to Global User Search - done.

--- src/containers/Users/view_user.jsx
+++ src/containers/Users/view_user.jsx
@@ -1,18 +1,32 @@
-import React, { useEffect } from 'react';
-import { Col, Container, Row, Card, CardBody } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import {Col, Container, Row, Card, CardBody, NavItem, NavLink, Nav, TabPane, Button, TabContent} from 'reactstrap';
 import { Link, useParams } from 'react-router-dom'
 import axios from 'axios'
+import classnames from "classnames";
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import ProgramsCard from './View/ProgramsCard'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { isEmpty } from '@/shared/helpers'
+import ViewUserProfile from "./View/profile";
+import ViewUserHistory from "./View/history";
+import ViewUserGiftCodesRedeemed from "./View/giftCodesRedeemed";
+import ViewUserChangeLogs from "./View/changeLogs";
+import { fetchRoles, fetchUserPrograms, fetchUserProgramRoles } from "@/shared/apiHelper"
+import {login} from "../App/auth";
 
 const queryClient = new QueryClient()
 
 const ViewUser = ({ organization }) => {
 
-    // console.log(organization)
+    const [userPrograms, setUserPrograms] = useState(null)
+    const [userProgramIds, setUserProgramIds] = useState(null)
+
+    // Tabs Panel
+    const [currentActiveTab, setCurrentActiveTab] = useState('1');
+    const togglePan = tab => {
+        if (currentActiveTab !== tab) setCurrentActiveTab(tab);
+    }
 
     const fetchUser = async (id) => {
         try {
@@ -22,6 +36,20 @@ const ViewUser = ({ organization }) => {
             throw new Error(`API error:${e?.message}`);
         }
     };
+
+    const getUserPrograms = () => {
+        fetchUserPrograms( organization.id, id )
+            .then( data => {
+                setUserPrograms(data);
+                setUserProgramIds(data.map(a => a.account_holder_id));
+            })
+    }
+
+    useEffect( () => {
+        if( organization.id )   {
+            getUserPrograms()
+        }
+    }, [])
 
     let { id } = useParams();
 
@@ -36,14 +64,6 @@ const ViewUser = ({ organization }) => {
         }
     )
 
-    // const refresh = () => {
-    //     remove()
-    // }
-
-    // React.useEffect(() => {
-    //     fetchUser( id );
-    // }, [fetchUser, id]);
-
     if (error) {
         return <p>Error loading user</p>;
     }
@@ -51,7 +71,6 @@ const ViewUser = ({ organization }) => {
         return <p>Loading...</p>;
     }
     if (isSuccess) {
-        // console.log(data)
         const fullName = `${data.first_name} ${data.last_name}`
         return (
             <Container className="dashboard">
@@ -59,7 +78,6 @@ const ViewUser = ({ organization }) => {
                     <Col md={12}>
                         <h3 className="page-title">{fullName}</h3>
                         <h3 className="page-subhead subhead"><Link className="" to="/">Home</Link> / <Link className="" to="/users">Users</Link> / {fullName}</h3>
-                        {/* <span onClick={refresh}>Refresh</span> */}
                     </Col>
                 </Row>
                 <Row>
@@ -67,102 +85,77 @@ const ViewUser = ({ organization }) => {
                         <Card>
                             <CardBody className='infoview'>
                                 <Row>
-                                    <Col md="6" lg="6" xl="6">
-                                        <h3 className="mb-4">User Profile</h3>
-                                    </Col>
-                                    <Col md="6" lg="6" xl="6" className="text-right">
-                                        <Link className="" to={`/users/edit/${data.id}`}>Edit</Link>
-                                    </Col>
-
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>First Name:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.first_name}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Last Name:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.last_name}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Roles:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <RenderUserRoles user={data} />
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Email:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.email}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Phone:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.phone}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Award Level:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.award_level}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Work Anniversary:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.work_anniversary}</p>
+                                    <Col md="7" >
+                                        <Nav tabs>
+                                            <NavItem>
+                                                <NavLink
+                                                    className={classnames({
+                                                        active:
+                                                            currentActiveTab === '1'
+                                                    })}
+                                                    onClick={() => {
+                                                        togglePan('1');
+                                                    }}
+                                                >
+                                                    User Profile
+                                                </NavLink>
+                                            </NavItem>
+                                            <NavItem>
+                                                <NavLink
+                                                    className={classnames({
+                                                        active:
+                                                            currentActiveTab === '2'
+                                                    })}
+                                                    onClick={() => {
+                                                        togglePan('2');
+                                                    }}
+                                                >
+                                                    History
+                                                </NavLink>
+                                            </NavItem>
+                                            <NavItem>
+                                                <NavLink
+                                                    className={classnames({
+                                                        active:
+                                                            currentActiveTab === '3'
+                                                    })}
+                                                    onClick={() => {
+                                                        togglePan('3');
+                                                    }}
+                                                >
+                                                    Gift Codes Redeemed
+                                                </NavLink>
+                                            </NavItem>
+                                            <NavItem>
+                                                <NavLink
+                                                    className={classnames({
+                                                        active:
+                                                            currentActiveTab === '4'
+                                                    })}
+                                                    onClick={() => {
+                                                        togglePan('4');
+                                                    }}
+                                                >
+                                                    Change Logs
+                                                </NavLink>
+                                            </NavItem>
+                                        </Nav>
                                     </Col>
                                 </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Department / Team:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.division}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Birthday:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.dob}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Employee Number:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.employee_number}</p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col md="2" lg="2" xl="2" sm="2" className='label'>
-                                        <p>Supervisor ID:</p>
-                                    </Col>
-                                    <Col md="10" lg="10" xl="10" sm="10">
-                                        <p>{data.supervisor_employee_number}</p>
-                                    </Col>
-                                </Row>
+                                <TabContent activeTab={currentActiveTab} className="tabContent">
+                                    <TabPane tabId="1" className="tabPane">
+                                        <ViewUserProfile data={data} organization={organization} />
+                                    </TabPane>
+                                    <TabPane tabId="2" className="tabPane">
+                                        <ViewUserHistory programs={userProgramIds} user={data} organization={organization} />
+                                    </TabPane>
+                                    <TabPane tabId="3" className="tabPane">
+                                        <ViewUserGiftCodesRedeemed programs={userProgramIds} user={data} organization={organization} />
+                                    </TabPane>
+                                    <TabPane tabId="4" className="tabPane">
+                                        <ViewUserChangeLogs programs={userProgramIds} user={data} organization={organization} />
+                                    </TabPane>
+                                </TabContent>
 
                             </CardBody>
                         </Card>
@@ -176,27 +169,6 @@ const ViewUser = ({ organization }) => {
             </Container>
         )
     }
-}
-
-const RenderUserRoles = ({ user }) => {
-    // console.log(user)
-    let rolesHtml = []
-    if (user.roles?.length > 0) {
-        user.roles.map(role => {
-            if (!role.is_program_role) {
-                console.log(role)
-                rolesHtml.push(<li>{role.name}</li>);
-            }
-        })
-        if (user.programRoles?.length > 0) {
-            user.programRoles.map(programRoles => {
-                programRoles.roles.map(programRole => {
-                    rolesHtml.push(<li>{programRole.name} in <a href={`/program/view/${programRoles.id}`}>{programRoles.name}</a></li>);
-                })
-            })
-        }
-    }
-    return rolesHtml;
 }
 
 const Wrapper = ({ organization }) => {
