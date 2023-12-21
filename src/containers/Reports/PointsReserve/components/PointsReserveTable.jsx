ded38fde73856e70674916d005474d538e1ad535
crypopanda430 <panda.kk1114@gmail.com>
2023-12-21 09:33:44 -0300

    resolve conflict

--- src/containers/Reports/PointsReserve/components/PointsReserveTable.jsx
+++ src/containers/Reports/PointsReserve/components/PointsReserveTable.jsx
@@ -55,7 +55,7 @@ useEffect(() => {
 
   const download = async (filterValues) => {
     let tmpFilter = clone(filterValues);
-    // tmpFilter.exportToCsv = 1;
+    tmpFilter.exportToCsv = 1;
     tmpFilter.limit = pageSize;
     tmpFilter.page = pageIndex+1;
     const response = await fetchApiDataExport(
