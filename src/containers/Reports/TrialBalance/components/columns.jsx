0394102c572631b6b637ae5b46327b9d8a172d64
Evgeny Monsvetov <emonsvetov@incento.com>
2023-12-21 23:10:51 +0300

    ICOV3-94. Admin General: Trial Balance - Done.

--- src/containers/Reports/TrialBalance/components/columns.jsx
+++ src/containers/Reports/TrialBalance/components/columns.jsx
@@ -3,45 +3,47 @@ import React, { useMemo } from 'react';
 
 export const TABLE_COLUMNS = [
   {
-    Header: "Account Holder",
-    accessor: "account_holder",
-    width: 130
+    Header: ' ',
+    columns: [
+      {
+        Header: "Account Holder",
+      },
+      {
+        Header: "Account",
+      },
+    ],
   },
   {
-    Header: "Account",
-    accessor: "account_id",
-    width: 130
+    Header: "Debit",
+    columns: [
+      {
+        Header: "Asset",
+        id: 1
+      },
+      {
+        Header: "Liability",
+        id: 2
+      },
+      {
+        Header: "Revenue",
+        id: 3
+      },
+    ],
   },
   {
-    Header: "Posting Amount",
-    accessor: "posting_amount",
-    width: 130
-  },
-  {
-    Header: "Quantity",
-    accessor: "qty",
-    width: 130
-  },
-  {
-    Header: "Credit/Debit",
-    accessor: "is_credit",
-    width: 130,
-    Cell: ({ value }) => (value ? "Credit" : "Debit")
-  },
-  {
-    Header: "Posting Timestamp",
-    accessor: "posting_timestamp",
-    width: 180
-  },
-  {
-    Header: "Account Type",
-    accessor: "account_type_name",
-    width: 130
-  },
-  {
-    Header: "Finance Type",
-    accessor: "finance_type_name",
-    width: 130
+    Header: 'Credit',
+    columns: [
+      {
+        Header: "Asset",
+      },
+      {
+        Header: "Liability",
+      },
+      {
+        Header: "Revenue",
+      },
+    ],
   }
-];
+]
+
 
