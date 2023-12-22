e35a8fa786b76639222cf03abae608cb146bbdee
vzotov <v.zotov@atlex.ru>
2023-12-22 16:30:41 +0300

    ICOV3-103. Admin Program: Annual Awards Summary

--- src/containers/Program/View/Reports/index.jsx
+++ src/containers/Program/View/Reports/index.jsx
@@ -9,12 +9,7 @@ import ParticipantAccountSubProgram from "../Reports/ParticipantAccountSubProgra
 import ProgramParticipantStatusSummary from "../Reports/ProgramParticipantStatusSummary";
 import SupplierRedemptionIndex from "../Reports/SupplierRedemptionIndex";
 import JournalDetailed from "../Reports/JournalDetail";
-import DepositTransfer from "../Reports/DepositTransfer";
-import DepositBalance from "../Reports/DepositBalance";
-import PointsPurchase from "../Reports/PointsPurchase";
-import ParticipantAccountSummary from '../Reports/ParticipantAccountSummary'
-import UserDetailsChangeLogs from "../Reports/UserDetailsChangeLog";
-import DeletedSocialWallPosts from '../Reports/DeletedSocialWallPosts'
+
 const ProgramReport = ({organization}) => {
   // Tabs Panel
   const [currentActiveTab, setCurrentActiveTab] = useState('1');
@@ -55,7 +50,7 @@ const ProgramReport = ({organization}) => {
         <Card>
           <CardBody className='infoview'>
             <Row>
-              <Col md="12" >
+              <Col md="7" >
                 <Nav tabs>
                   <NavItem>
                     <NavLink
@@ -96,83 +91,17 @@ const ProgramReport = ({organization}) => {
                       Journal Detail
                     </NavLink>
                   </NavItem>
-                  <NavLink
-                      className={classnames({
-                          active:
-                              currentActiveTab === '4'
-                      })}
-                      onClick={() => {
-                          togglePan('4');
-                      }}
-                  >
-                      Supplier Redemption
-                  </NavLink>
-                  <NavLink
-                      className={classnames({
-                        active:
-                            currentActiveTab === '5'
-                      })}
-                      onClick={() => {
-                        togglePan('5');
-                      }}
-                  >
-                    Deposit Transfers
-                  </NavLink>
-                  <NavLink
-                      className={classnames({
-                        active:
-                            currentActiveTab === '6'
-                      })}
-                      onClick={() => {
-                        togglePan('6');
-                      }}
-                  >
-                    Deposit Balance
-                  </NavLink>
-                  <NavLink
-                      className={classnames({
-                        active:
-                            currentActiveTab === '7'
-                      })}
-                      onClick={() => {
-                        togglePan('7');
-                      }}
-                  >
-                    Point Purchase
-                  </NavLink>
-                  <NavLink
-                      className={classnames({
-                        active:
-                            currentActiveTab === '8'
-                      })}
-                      onClick={() => {
-                        togglePan('8');
-                      }}
-                  >
-                    Participant Account Summary
-                  </NavLink>
-                  <NavLink
-                      className={classnames({
-                        active:
-                            currentActiveTab === '9'
-                      })}
-                      onClick={() => {
-                        togglePan('9');
-                      }}
-                  >
-                    User Detail Change Log
-                  </NavLink>
-                  <NavLink
-                      className={classnames({
-                        active:
-                            currentActiveTab === '10'
-                      })}
-                      onClick={() => {
-                        togglePan('10');
-                      }}
-                  >
-                      Deleted Social Wall Posts
-                  </NavLink>
+                    <NavLink
+                        className={classnames({
+                            active:
+                                currentActiveTab === '4'
+                        })}
+                        onClick={() => {
+                            togglePan('4');
+                        }}
+                    >
+                        Supplier Redemption
+                    </NavLink>
                 </Nav>
               </Col>
             </Row>
@@ -192,48 +121,12 @@ const ProgramReport = ({organization}) => {
                   <JournalDetailed program={program}/>
                 }
               </TabPane>
-              <TabPane tabId="4">
-                  {
-                      currentActiveTab != 4 ? 'Loading...' :
-                          <SupplierRedemptionIndex program={program}/>
-                  }
-              </TabPane>
-              <TabPane tabId="5">
-                {
-                  currentActiveTab != 5 ? 'Loading...' :
-                      <DepositTransfer program={program}/>
-                }
-              </TabPane>
-              <TabPane tabId="6">
-                {
-                  currentActiveTab != 6 ? 'Loading...' :
-                      <DepositBalance program={program}/>
-                }
-              </TabPane>
-              <TabPane tabId="7">
-                {
-                  currentActiveTab != 7 ? 'Loading...' :
-                      <PointsPurchase program={program}/>
-                }
-              </TabPane>
-              <TabPane tabId="8">
-                {
-                  currentActiveTab != 8 ? 'Loading...' :
-                      <ParticipantAccountSummary program={program}/>
-                }
-              </TabPane>
-              <TabPane tabId="9">
-                {
-                  currentActiveTab != 9 ? 'Loading...' :
-                      <UserDetailsChangeLogs program={program}/>
-                }
-              </TabPane>
-              <TabPane tabId="10">
-                {
-                  currentActiveTab != 10 ? 'Loading...' :
-                      <DeletedSocialWallPosts program={program}/>
-                }
-              </TabPane>
+                <TabPane tabId="4">
+                    {
+                        currentActiveTab != 4 ? 'Loading...' :
+                            <SupplierRedemptionIndex program={program}/>
+                    }
+                </TabPane>
             </TabContent>
           </CardBody>
         </Card>
