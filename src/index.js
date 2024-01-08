2b2dcd4ae47bd4adcf011dc51747bbb2fa0b5cbe
vzotov <v.zotov@atlex.ru>
2024-01-08 21:45:03 +0300

    ICOV3-103. Admin Program: Annual Awards Summary

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';  // Import from react-dom/client
 import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+const rootElement = document.getElementById('root');
+
+const root = createRoot(rootElement);
+
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+
+root.render(
+        <App />
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// If you want your app to work offline and load faster,
+// you can change unregister() to register() below.
+// Note: This comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
