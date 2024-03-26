fe34db324eabea77f133c7a76a47326699603cdc
Oleg Ganshonkov <oganshonkov@incentco.com>
2024-03-26 18:01:19 +0300

    ICOV3-220. Budget Comparison - Done.

--- src/containers/Program/View/Reports/BudgetComparison/components/columns.jsx
+++ src/containers/Program/View/Reports/BudgetComparison/components/columns.jsx
@@ -1,14 +1,65 @@
 import { toPoints} from '@/shared/helpers'
+import { monthToString} from '@/shared/helpers'
+import React, {useMemo} from "react";
 export const TABLE_COLUMNS = [
   {
     Header: "Month",
     accessor: "month",
+    Cell: ({row, value}) => {
+      return monthToString(value);
+    },
+    Footer: (info) => {
+      const {rows, flatRows} = info;
+      const totalValue = useMemo(
+        () => rows.reduce((sum, row) => Number(row.values.awarded) + sum, 0),
+        [rows],
+      );
+      return <span>Total</span>;
+    },
   },
   {
     Header: "Budget",
-    accessor: "budget_value",
-    Cell: function ($row, $value) {
-  return toPoints($value);
-},
+    accessor: "budget",
+    Cell: ({row, value}) => {
+      return toPoints(value);
+    },
+    Footer: (info) => {
+      const {rows, flatRows} = info;
+      const totalValue = useMemo(
+        () => rows.reduce((sum, row) => Number(row.values.budget) + sum, 0),
+        [rows],
+      );
+      return <span>{`$${parseFloat(totalValue).toFixed(2)}`}</span>;
+    },
+  },
+  {
+    Header: "Actual",
+    accessor: "awarded",
+    Cell: ({row, value}) => {
+      return toPoints(value);
+    },
+    Footer: (info) => {
+      const {rows, flatRows} = info;
+      const totalValue = useMemo(
+        () => rows.reduce((sum, row) => Number(row.values.awarded) + sum, 0),
+        [rows],
+      );
+      return <span>{`$${parseFloat(totalValue).toFixed(2)}`}</span>;
+    },
+  },
+  {
+    Header: "Variance",
+    accessor: "variance",
+    Cell: ({row, value}) => {
+      return toPoints(Math.abs(value));
+    },
+    Footer: (info) => {
+      const {rows, flatRows} = info;
+      const totalValue = useMemo(
+        () => rows.reduce((sum, row) => Math.abs(Number(row.values.variance)) + sum, 0),
+        [rows],
+      );
+      return <span>{`$${parseFloat(totalValue).toFixed(2)}`}</span>;
+    },
   },
 ];
