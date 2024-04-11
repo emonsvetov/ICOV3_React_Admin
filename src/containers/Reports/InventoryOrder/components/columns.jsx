41cef1af24c4037d1c401e5cc240acf57cf02c91
andreamelkov <andreamelkov61@gmail.com>
2024-04-11 18:05:16 +0300

    ICOV3-374. fixed Inventory Order report.

--- src/containers/Reports/InventoryOrder/components/columns.jsx
+++ src/containers/Reports/InventoryOrder/components/columns.jsx
@@ -6,23 +6,22 @@ export const TABLE_COLUMNS = [
   {
     Header: "Merchant",
     accessor: "merchant_name",
-    width: 130,
-    rowSpan: 2
+    width: 250,
   },
   {
     Header: "Denomination",
     accessor: "denomination",
-    width: 130
+    width: 250,
   },
   {
     Header: "# in Inventory",
     accessor: "count",
-    width: 130
+    width: 250,
   },
   {
     Header: "2-Week Target",
     accessor: "optimal_value",
-    width: 130
+    width: 250,
   },
 ]
 
